dbb20b842222a793d7c60ac2719dce1fc76de449
rakeshreda <rakesh@redappletech.com>
2022-08-08 15:29:40 +0530

    route handeler implimented globaly

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ dotenv.config({"path":"./config.env"});
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+ 
 
 app.use("/user/api",userRouter);
+app.all("*",(req,res,next)=>{
+    res.status(404).json({status:"fail",message:`Can't Find ${req.originalUrl} on this server`})
+})
 const PORT=process.env.PORT||5000;
 
 
